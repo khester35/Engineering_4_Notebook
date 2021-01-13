800bac6a02b4ca46abc887d908ff8cd0825dd34c
khester35 <khester35@charlottesvilleschools.org>
2021-01-13 18:36:25 +0000

    headless.py

--- Python/headless.py
+++ Python/headless.py
@@ -35,7 +35,7 @@ draw.rectangle((0,0,width,height), outline=0, fill=0)
 # Draw some shapes.
 # First define some constants to allow easy resizing of shapes.
 padding = 2
-shape_width = 20
+shape_width = 120
 top = 0 # top = padding
 bottom = height-padding
 # Move left to right keeping track of the current x position for drawing shapes.
