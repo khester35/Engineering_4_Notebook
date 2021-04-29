749d2f4b0f30925ec8f48ad34a44328cd1747f9e
khester35 <khester35@charlottesvilleschools.org>
2021-04-29 16:34:51 +0100

    camera_test01.py

---
+++ Python/Pics/camera_test01.py
@@ -0,0 +1,11 @@
+import time
+import picamera
+
+with picamera.PiCamera() as camera:
+    camera.resolution = (1024, 768)
+    camera.start_preview()
+    print("running")
+    # Camera warm-up time
+    time.sleep(2)
+    camera.capture('test2.jpg')
+    print("done")
